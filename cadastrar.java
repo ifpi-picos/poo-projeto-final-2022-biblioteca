86c309f21b3aefde98719d8feee429d085bb1fd8
Marciely Ferreira <89710284+marcielysilva@users.noreply.github.com>
2022-11-20 17:03:03 -0300

    atualizaçaõ

---
+++ cadastrar.java
@@ -0,0 +1,10 @@
+public class Cadastrar {
+    private String titulo;
+    private String autor;
+    private String areaConhecimento;
+    private Data dataPublicacao;
+    private int qntCopias;
+
+    
+
+}
