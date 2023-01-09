ac901c266630d51e1c96f1c7c3e54ca2038551f8
Marciely Ferreira <89710284+marcielysilva@users.noreply.github.com>
2023-01-08 21:24:28 -0300

    atualizando

--- .github/cadastrarUsuario.java
+++ .github/cadastrarUsuario.java
@@ -19,11 +19,6 @@ public class cadastrarUsuario{
     private String getEmail(){
         return email;
     }
-    
-    private Date getdataNascimento(){
-        return dataNasciemnto;
-    }
-
     private void setEndereco(Endereco endereco){
         this.endereco = endereco;
     }
@@ -38,18 +33,6 @@ public class cadastrarUsuario{
         private String cidade;
         private String estado;
         private String pais;
-    }
-    public String getRua(){
-        return rua;
-    }
-    public void setRua(String rua){
-        this.rua = rua;
-    }
-    public String getNumero(){
-        return numero;
-    }
-    public String setNumero(String numero){
-        this.numero = numero;
     }
 
 }
